1d96872059760f304a2dff42e9c7991867cbfd45
mowhamd1999 <mohammaderfansadeghiani@gmail.com>
2024-06-27 18:52:08 +0330

    updated

--- src/Pages/ShoppingCart/ShoppingCart.jsx
+++ src/Pages/ShoppingCart/ShoppingCart.jsx
@@ -3,6 +3,8 @@ import style from "./ShoppingCart.module.css";
 import { CartContext } from "../../context/context-product/ContextProduct";
 import Cart from "../../Components/Cart/Cart";
 import SideBar from "../../Components/Cart/SideBar";
+
+import { Link } from "react-router-dom";
 const ShoppingCart = () => {
   const { state, dispatch } = useContext(CartContext);
   const [product, setProduct] = useState(state.buys);
@@ -25,6 +27,12 @@ const ShoppingCart = () => {
           </div>
         </div>
       )}
+      <div className={style.empty_div}>
+        <h2 className={style.h2}>محصولی در سبد خرید شما یافت نشد</h2>
+        <Link className="join" to={'/products'} >
+              <p className={style.link}>رفتن به صفحه محصولات</p>
+        </Link>
+      </div>
     </div>
   );
 };
