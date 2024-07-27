43b2e687ed76b6fdfdba3daa8c60c25eee30e896
mowhamd1999 <mohammaderfansadeghiani@gmail.com>
2024-07-27 13:26:07 +0330

    updated

--- src/Components/MiniBuyBox/MiniBuyBox.jsx
+++ src/Components/MiniBuyBox/MiniBuyBox.jsx
@@ -2,24 +2,23 @@ import React, { useContext, useEffect, useState } from "react";
 import style from "./MiniBuyBox.module.css";
 
 import { CiCircleInfo } from "react-icons/ci";
-
+import { CiTrash } from "react-icons/ci";
 import { CartContext } from "../../context/context-product/ContextProduct";
 import Notifi from "../notificationModule/Notifi";
 const MiniBuyBox = ({ product }) => {
   const { state, dispatch } = useContext(CartContext);
-  const [module , setModule] = useState(false)
+  const [module, setModule] = useState(false);
   const onhide = () => {
-    setModule(false)
-  }
-
+    setModule(false);
+  };
 
   const add = () => {
-    dispatch({ type: "Add_item", payload: data })
-    setModule(true)
+    dispatch({ type: "Add_item", payload: data });
+    setModule(true);
     setTimeout(() => {
-      setModule(false)
+      setModule(false);
     }, 2000);
-  }
+  };
 
   const formatNumber = (num) => {
     if (!num) return "";
@@ -28,9 +27,9 @@ const MiniBuyBox = ({ product }) => {
   useEffect(() => {
     console.log(state);
   }, [state]);
-  const quantity = state.buys.find(item=> item.id === product.id)
-  console.log(quantity)
-  const data = product
+  const quantity = state.buys.find((item) => item.id === product.id);
+  console.log(quantity);
+  const data = product;
   return (
     <div className={style.bg}>
       <div className={style.sell_container_top}>
@@ -52,27 +51,44 @@ const MiniBuyBox = ({ product }) => {
       {state.buys.find((item) => item.id === product.id) ? (
         <div className={style.box}>
           <div className={style.quantity_box}>
-            <button 
-            className={style.increase_btn}
-            onClick={() => dispatch({type:'decrease_item', payload:quantity})}
-              >-</button>
+            {quantity.quantity > 1 &&
+              <button
+                className={style.increase_btn}
+                onClick={() =>
+                  dispatch({ type: "decrease_item", payload: quantity })
+                }
+              >
+                -
+              </button>
+            }
+            {quantity.quantity === 1 && 
+              <button
+                className={style.increase_btn}
+                onClick={() =>
+                  dispatch({ type: "decrease_item", payload: quantity })
+                }
+              >
+                <CiTrash className={style.delete_btn}/>
+              </button>
+            }
             <p className={style.increase_btn}>{quantity.quantity}</p>
-            <button 
-            className={style.increase_btn}
-            onClick={() => dispatch({type:'increase_item', payload:quantity})}
-            >+</button>
+            <button
+              className={style.increase_btn}
+              onClick={() =>
+                dispatch({ type: "increase_item", payload: quantity })
+              }
+            >
+              +
+            </button>
           </div>
           <p className={style.increase_title}>تعداد در سبد خرید</p>
         </div>
       ) : (
-        <button
-          onClick={ add }
-          className={style.add_btn}
-        >
+        <button onClick={add} className={style.add_btn}>
           افزودن به سبد خرید
         </button>
       )}
-      {module == true && <Notifi onhide={onhide} product={product} />}
+      {module === true && <Notifi onhide={onhide} product={product} />}
     </div>
   );
 };
